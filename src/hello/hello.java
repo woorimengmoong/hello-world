580a67089e4b7ec33be689632430ecd416805a9e
seoil-u <seoil-u@localhost>
2023-07-27 01:47:51 +0900

    메시지 추가

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -3,5 +3,7 @@ package hello;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("helloworld");
+		System.out.println("안녕월드");
+System.out.println("hihi");
 	}
 }
